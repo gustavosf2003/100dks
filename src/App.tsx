98239da9f9bafc65bddebda7a69444232645f134
gustavosf2003 <gustavoferreira.dev@gmail.com>
2025-10-18 23:53:17 -0300

    Refactor App component by removing unused providers and components, simplifying the structure to focus on routing.

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { AuthProvider } from "@/contexts/AuthContext";
-
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <AuthProvider>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="*" element={<div>Hello World</div>} />
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
-    </AuthProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<div>Hello World</div>} />
+      </Routes>
+    </BrowserRouter>
   </QueryClientProvider>
 );
 
